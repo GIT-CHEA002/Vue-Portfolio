062edcbd5d6730415f7cb95ef4ec986dd1099c34
GIT-CHEA002 <chhunsokchea317@gmail.com>
2026-06-13 02:17:15 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterView } from "vue-router";
-import Navbar from "./components/navbar/Navbar.vue";
-import Footer from "./components/footer/Footer.vue";
+import Navbar from "../src/components/navbar/Navbar.vue";
+import Footer from "../src/components/footer/Footer.vue";
 
 </script>
 <template>
