fe8b8f6c60489ab3ad13943979fd52bdd703a1af
GIT-CHEA002 <chhunsokchea317@gmail.com>
2026-06-13 13:27:45 +0700

    update app , root of the project

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,9 @@
 import { RouterView } from "vue-router";
 import Navbar from "@/components/navbar/Navbar.vue";
 import Footer from "@/components/footer/Footer.vue";
-
 </script>
 <template>
-  <div class="w-full">
     <Navbar />
     <RouterView />
     <Footer />
-  </div>
-
 </template>
