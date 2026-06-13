5026db1f9d00cc2d3245a91889861a85c4cbf17e
GIT-CHEA002 <chhunsokchea317@gmail.com>
2026-06-13 13:53:45 +0700

    remove navbar link

--- src/components/navbar/HireLink.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-<template>
-  <div
-    class="border px-2 hidden md:block bg-cyan-600 text-white dark:bg-sky-400 border-cyan-600 dark:border-sky-400  rounded-sm dark:rounded-sm">
-    <RouterLink to="/contact" class="text-sm font-semibold tracking-wide text-white">HIRE ME</RouterLink>
-  </div>
-</template>
